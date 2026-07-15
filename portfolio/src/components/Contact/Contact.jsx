908e5bc0b5563e7c3da6dc6d8154d60cff236c2d
Jeetvk18 <prajapatj969@gmail.com>
2026-07-16 01:58:05 +0530

    Update API URL

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -37,7 +37,7 @@ function Contact() {
     try{
 
       const res = await axios.post(
-        "http://localhost:5000/api/contact",
+        "https://personal-portfolio-jatin.onrender.com/api/contact",
         formData
       );
 
--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://personal-portfolio-jatin.vercel.app",
+  })
+);
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
